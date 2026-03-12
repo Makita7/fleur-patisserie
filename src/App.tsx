5206856791d50eec3c95effd677d86f0d7812d11
Sofía Makita <sophiamakita@gmail.com>
2026-03-12 18:59:47 -0300

    making footer

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,26 @@ import Reserve from './pages/Reserve.jsx'
 
 //Components
 import NavMobile from './components/NavMobile'
+import Footer from './components/Footer'
 
 
 function App() {
 
 
   return (
-    <BrowserRouter >
-      <NavMobile/>
-      <Routes>
-        <Route path='/' element={ <Menu/> } />
-        <Route path='/garden' element={ <Garden/> } />
-        <Route path='/about' element={ <About/> } />
-        <Route path='/visit' element={ <Visit/> } />
-        <Route path='/reserve' element={ <Reserve/> } />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter >
+        <NavMobile/>
+        <Routes>
+          <Route path='/' element={ <Menu/> } />
+          <Route path='/garden' element={ <Garden/> } />
+          <Route path='/about' element={ <About/> } />
+          <Route path='/visit' element={ <Visit/> } />
+          <Route path='/reserve' element={ <Reserve/> } />
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
+    </>
   )
 }
 
