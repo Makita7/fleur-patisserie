84e2ea2a50d10f80bfcdd807c2c8ce1e2beacedb
Sofía Makita <sophiamakita@gmail.com>
2026-03-17 13:12:26 -0300

    garden translation

---
+++ src/data/gardenData.js
@@ -0,0 +1,24 @@
+
+
+export const garden = {
+    eng: {
+        title: "Our Garden",
+        textHeader: "A quiet corner to enjoy pastries, coffee, and fresh air.",
+        titleUnwind: "Perfect for Unwinding",
+        textUnwind: "Our garden inspires the flavors of the patisserie. Seasonal fruits, fresh herbs, and natural ingredients help shape many of our pastries and drinks. It’s a simple idea—fresh ingredients, baked with care, and enjoyed just a few steps away in the garden.",
+        titleSeason: "Changing With the Seasons",
+        textSeason: "The garden changes throughout the year. In spring and summer, the space fills with flowers and sunlight. During autumn, warm drinks and golden light make it the perfect cozy corner. Each season brings a new atmosphere to enjoy.",
+        titleVisit: "Visit Us",
+        textVisit: "The garden is open during our regular store hours and available at both of our locations.",
+    },
+    spa: {
+        title: "Nuestro Jardín",
+        textHeader: "Un rincón tranquilo para disfrutar pastelería, café y aire fresco.",
+        titleUnwind: "Perfecto para Desconectar",
+        textUnwind: "Nuestro jardín inspira muchos de los sabores de la patisserie. Frutas de estación, hierbas frescas e ingredientes naturales ayudan a dar forma a muchas de nuestras pastelerías y bebidas. Es una idea simple: ingredientes frescos, preparados con cuidado y disfrutados a solo unos pasos del jardín.",
+        titleSeason: "Cambiando con las Estaciones",
+        textSeason: "El jardín cambia a lo largo del año. En primavera y verano, el espacio se llena de flores y luz natural. Durante el otoño, las bebidas cálidas y la luz dorada lo convierten en el rincón perfecto para disfrutar con calma. Cada estación trae una nueva atmósfera para descubrir.",
+        titleVisit: "Visitanos",
+        textVisit: "El jardín está abierto durante nuestro horario habitual y se encuentra disponible en ambas sucursales.",
+    }
+}
--- src/pages/Garden.jsx
+++ src/pages/Garden.jsx
@@ -1,4 +1,5 @@
-
+import { garden } from '../data/gardenData'
+import { useLanguage } from "../components/LanguageContext";
 import './garden.css'
 
 // Mobile Images
@@ -16,6 +17,10 @@ import GardenVisitWeb from '../assets/web-images/visit-us-web.png'
 //Components
 
 export default function Garden() {
+
+    const { lang } = useLanguage();
+    const gardenData = garden[lang];
+
     return (
         <div className='garden'>
             <div className='block md:flex md:px-[5rem] justify-center items-center'>
@@ -24,8 +29,8 @@ export default function Garden() {
                     <img src={GardenHeroWeb} className=" garden-hero-img-desktop" />
                 </div>
                 <div className='px-8 mt-8 mb-14 md:mb-0 md:w-1/2 md:pl-[4rem] md:pr-[10rem] text-center md:text-left md:my-0'>
-                    <h1 className='title-green'>Our Garden</h1>
-                    <p>A quiet corner to enjoy pastries, coffee, and fresh air.</p>
+                    <h1 className='title-green'>{gardenData.title}</h1>
+                    <p>{gardenData.textHeader}</p>
                     <hr className='line-green' />
                 </div>
             </div>
@@ -33,8 +38,8 @@ export default function Garden() {
             <div className='block md:flex md:px-[5rem] justify-center items-center md:h-[60rem]'>
                 <img src={GardenMacaroonsMobile} className="block md:hidden" />
                 <div className='mt-8 mb-12 mx-10 md:my-0 md:w-1/2 md:pl-[10rem] md:pr-[4rem]'>
-                    <h3 className='title-red'>Perfect for Unwinding</h3>
-                    <p>Our garden inspires the flavors of the patisserie. Seasonal fruits, fresh herbs, and natural ingredients help shape many of our pastries and drinks. It’s a simple idea—fresh ingredients, baked with care, and enjoyed just a few steps away in the garden.</p>
+                    <h3 className='title-red'>{gardenData.titleUnwind}</h3>
+                    <p>{gardenData.textUnwind}</p>
                 </div>
                 <img src={GardenMacaroonsWeb} className="hidden md:block garden-macaroon-desktop" />
             </div>
@@ -43,8 +48,8 @@ export default function Garden() {
                 <img src={GardenTableMobile} className="block md:hidden" />
                 <img src={GardenTableWeb} className="hidden md:block garden-img-table-desktop" />
                 <div className='mt-8 mb-12 mx-10 md:my-0 md:w-1/2 md:pl-[4rem] md:pr-[10rem]'>
-                    <h3 className='title-green'>Changing With the Seasons</h3>
-                    <p>The garden changes throughout the year. In spring and summer, the space fills with flowers and sunlight. During autumn, warm drinks and golden light make it the perfect cozy corner. Each season brings a new atmosphere to enjoy.</p>
+                    <h3 className='title-green'>{gardenData.titleSeason}</h3>
+                    <p>{gardenData.textSeason}</p>
                 </div>
             </div>
 
@@ -52,8 +57,8 @@ export default function Garden() {
                 <img src={GardenVisitMobile} className="block md:hidden" />
                 <img src={GardenVisitWeb} className="hidden md:block menu-img-visit mx-auto" />
                 <div className='absolute inset-0 flex flex-col items-center justify-center text-white text-center md:mr-20'>
-                    <h3>Visit Us</h3>
-                    <p className='px-12 md:px-0 md:w-1/3'>The garden is open during our regular store hours and available at both of our locations.</p>
+                    <h3>{gardenData.titleVisit}</h3>
+                    <p className='px-12 md:px-0 md:w-1/3'>{gardenData.textVisit}</p>
                 </div>
             </div>
         </div>
